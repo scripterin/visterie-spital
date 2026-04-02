92448263bbc58c87ee15df7a011c9d58087f1415
scripterin <bogdanro777@gmail.com>
2026-04-02 16:20:59 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,28 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.discordapp.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/_next/image(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
