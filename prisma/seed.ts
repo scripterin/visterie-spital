5294af98e7a9b76e6047ed7d848c0f981ca53a15
scripterin <bogdanro777@gmail.com>
2026-04-05 23:28:36 +0300

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,13 +11,16 @@ async function main() {
     { discordId: "702150314643554304", callsign: "M-001", rol: "admin", name: "Darrin Rodriguez" },
     { discordId: "285446821533188101", callsign: "M-002", rol: "user", name: "Shades Antonio" },
     { discordId: "838419996556001341", callsign: "M-003", rol: "user", name: "Alex Tudorescu" },
-    { discordId: "377076232254390273", callsign: "M-004", rol: "user", name: "Neiconi Petrica" },
+    { discordId: "", callsign: "M-004", rol: "user", name: "N/A" },
     { discordId: "323874759962132502", callsign: "M-005", rol: "user", name: "Brown Allanon" },
-    { discordId: "556128906357374996", callsign: "M-006", rol: "user", name: "Mihail Parvu" },
-    { discordId: "889089062844137532", callsign: "M-007", rol: "user", name: "Rares Barbu" },
-    { discordId: "1142522913510146129", callsign: "M-008", rol: "user", name: "Paduraru David" },
-    { discordId: "793944399913680916", callsign: "M-009", rol: "user", name: "Marius Mark" },
-    { discordId: "", callsign: "M-010", rol: "user", name: "N/A" },
+    { discordId: "377076232254390273", callsign: "M-006", rol: "user", name: "Neiconi Petrica" },
+    { discordId: "556128906357374996", callsign: "M-007", rol: "user", name: "Mihail Parvu" },
+    { discordId: "", callsign: "M-008", rol: "user", name: "N/A" },
+    { discordId: "889089062844137532", callsign: "M-009", rol: "user", name: "Rares Barbu" },
+    { discordId: "1142522913510146129", callsign: "M-010", rol: "user", name: "Paduraru David" },
+    { discordId: "793944399913680916", callsign: "M-011", rol: "user", name: "Marius Mark" },
+    { discordId: "", callsign: "M-012", rol: "user", name: "N/A" },
+
   ];
 
   for (const user of whitelistUsers) {
